4ba9a639a1e0b6c2bbe7bea8b9ceb7f1bb97b395
Khaled Shaaban <kshaaban@outlook.com>
2018-04-03 15:36:03 -0400

    chore: create separete client/server folders in src

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -5,16 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
