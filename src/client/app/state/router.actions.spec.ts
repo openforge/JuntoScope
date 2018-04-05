43ce4e5d8c8ec79c57e7241de206d3883382e53e
Khaled Shaaban <kshaaban@outlook.com>
2018-04-05 13:12:30 -0400

    feat(core): define router actions and reducer

---
+++ src/client/app/state/router.actions.spec.ts
@@ -0,0 +1,35 @@
+import * as RouterActions from './router.actions';
+
+describe('Router Actions', () => {
+  describe('Go Action', () => {
+    it('should create action', () => {
+      const payload: RouterActions.NavigationOptions = { path: ['/login'] };
+      const action = new RouterActions.GoAction(payload);
+
+      expect(action).toEqual({
+        type: RouterActions.RouterActionTypes.GO,
+        payload,
+      });
+    });
+  });
+
+  describe('Back Action', () => {
+    it('should create action', () => {
+      const action = new RouterActions.BackAction();
+
+      expect(action).toEqual({
+        type: RouterActions.RouterActionTypes.BACK,
+      });
+    });
+  });
+
+  describe('Forward Action', () => {
+    it('should create action', () => {
+      const action = new RouterActions.ForwardAction();
+
+      expect(action).toEqual({
+        type: RouterActions.RouterActionTypes.FORWARD,
+      });
+    });
+  });
+});
