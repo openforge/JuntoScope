94a379cf2fd1a2dadb7d5fd6e4e28be46cd45222
Khaled Shaaban <kshaaban@outlook.com>
2018-04-05 13:16:56 -0400

    feat(core): connect NgRx modules with base app state

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,13 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
+import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { reducers, metaReducers, initialState } from './state/app.state';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule.forRoot(),
+    StoreModule.forRoot(reducers, { metaReducers, initialState }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
