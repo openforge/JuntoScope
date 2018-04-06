be43e22a1cc11727976a0a3c989ab31ee353f9dd
Khaled Shaaban <kshaaban@outlook.com>
2018-04-05 23:37:49 -0400

    feat(auth): create auth-related route guards

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -10,15 +10,19 @@ import {
 import { CustomSerializer } from './state/router.reducer';
 import { RouterFacade } from './state/router.facade';
 import { NotFoundComponent } from './not-found.component';
+import { UnAuthGuard } from './un-auth.guard';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: './authentication/authentication.module#AuthenticationModule',
+    canActivate: [UnAuthGuard],
   },
   {
     path: 'scoping',
     loadChildren: './scoping/scoping.module#ScopingModule',
+    canActivate: [AuthGuard],
   },
   {
     path: '**',
