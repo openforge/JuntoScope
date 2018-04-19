8f8a0817527b98ef10532bae820f1dbfd91277e9
daftclaud <cdsmileyone@gmail.com>
2018-04-19 17:40:29 -0400

    feat(navigation): Tabs only show when user is authenticated.

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+import { map } from 'rxjs/operators';
+
+import { AuthFacade } from './authentication/state/auth.facade';
+import { AuthCase } from './authentication/state/auth.reducer';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(private authFacade: AuthFacade) {}
+
+  authenticaded$ = this.authFacade.authState$.pipe(
+    map(authState => authState === AuthCase.AUTHENTICATED)
+  );
+
   title = 'app';
 }
