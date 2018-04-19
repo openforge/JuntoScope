306369d808e1b47def2698ee8bd70e6cfc009332
Hamada <Hamada@Hamada-MBP.local>
2018-04-19 12:58:51 -0400

    Added Presentational components for the Projects feature

---
+++ src/client/app/projects/components/projects-list/projects-list.component.ts
@@ -0,0 +1,11 @@
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-projects-list',
+  templateUrl: './projects-list.component.html',
+  styleUrls: ['./projects-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProjectsListComponent {
+  @Input() projects: any[];
+}
