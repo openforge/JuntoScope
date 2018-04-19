ac0e8a35dd7d94bbda168c0a5228a0a0abff6a41
Hamada <Hamada@Hamada-MBP.local>
2018-04-19 11:25:20 -0400

    Created Projects feature skeleton

---
+++ src/client/app/projects/projects.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProjectsRoutingModule } from './projects-routing.module';
+
+@NgModule({
+  imports: [CommonModule, ProjectsRoutingModule],
+  declarations: [],
+})
+export class ProjectsModule {}
