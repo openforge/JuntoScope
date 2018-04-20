f05dd2dc8ff8b41595e7bdafb0a556d0253c667a
Fernando del olmo <fer.olmo92@gmail.com>
2018-04-20 13:55:47 +0200

    Added manage connections skeleton

--- src/client/app/settings/containers/settings/settings.component.ts
+++ src/client/app/settings/containers/settings/settings.component.ts
@@ -1,13 +1,43 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { TakeUntilDestroy, untilDestroyed } from 'ngx-take-until-destroy';
 
+import { map, filter, withLatestFrom, take } from 'rxjs/operators';
+
+import { AuthFacade } from '../../../authentication/state/auth.facade';
+import { AuthCase } from '../../../authentication/state/auth.reducer';
+import { RouterFacade } from '../../../state/router.facade';
+
+@TakeUntilDestroy()
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnDestroy {
+  user$ = this.authFacade.user$;
 
-  constructor() {}
+  private logoutRedirect$ = this.authFacade.authState$.pipe(
+    untilDestroyed(this),
+    filter(authState => authState === AuthCase.NOT_AUTHENTICATED),
+    withLatestFrom(this.routerFacade.queryParams$)
+  );
+
+  constructor(
+    private authFacade: AuthFacade,
+    private routerFacade: RouterFacade
+  ) {}
 
   ngOnDestroy() {}
+
+  logout() {
+    this.authFacade.logout();
+
+    this.logoutRedirect$.pipe(take(1)).subscribe(() => {
+      this.routerFacade.navigate({ path: ['/login'] });
+    });
+  }
+
+  navigateManageConnections() {
+    this.routerFacade.navigate({ path: ['/settings/manage-connections'] });
+  }
 }
--- src/client/app/settings/settings-routing.module.ts
+++ src/client/app/settings/settings-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SettingsComponent } from './containers/settings/settings.component';
+import { ManageConnectionsComponent } from './containers/manage-connections/manage-connections.component';
 
 const routes: Routes = [
-    {
+  {
     path: '',
-    pathMatch: 'full',
-    redirectTo: 'settings',
+    component: SettingsComponent,
   },
   {
-    path: 'settings',
-    component: SettingsComponent,
+    path: 'manage-connections',
+    component: ManageConnectionsComponent,
   },
 ];
 
