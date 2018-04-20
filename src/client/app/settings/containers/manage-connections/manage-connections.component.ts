44f2d95dad8e7b0878306a67d956aedcfc3a2672
Fernando del olmo <fer.olmo92@gmail.com>
2018-04-20 13:55:47 +0200

    Added manage connections skeleton

---
+++ src/client/app/settings/containers/manage-connections/manage-connections.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-connections',
+  templateUrl: './manage-connections.component.html',
+  styleUrls: ['./manage-connections.component.scss'],
+})
+export class ManageConnectionsComponent implements OnDestroy {
+
+  constructor(
+
+  ) {}
+
+  ngOnDestroy() {}
+}
--- src/client/app/settings/settings.module.ts
+++ src/client/app/settings/settings.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsComponent } from './containers/settings/settings.component';
+import { ManageConnectionsComponent } from './containers/manage-connections/manage-connections.component';
 
 @NgModule({
   imports: [CommonModule, SettingsRoutingModule],
-  declarations: [SettingsComponent],
+  declarations: [SettingsComponent, ManageConnectionsComponent],
 })
 export class SettingsModule {}
