f7cfd7d1807e3a81519c56a729a4eec93b91b356
Meredith Alcorn <alcornmeredith@gmail.com>
2018-04-22 15:06:44 -0400

    feat(app-routing): add routing for projects-dashboard and add-projects

--- src/client/app/projects/projects-routing.module.ts
+++ src/client/app/projects/projects-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectsDashboardComponent } from './containers/projects-dashboard/projects-dashboard.component';
+import { AddProjectsComponent } from './containers/add-projects/add-projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ProjectsDashboardComponent,
+  },
+  {
+    path: 'add-projects',
+    component: AddProjectsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
