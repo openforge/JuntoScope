2a5756c53ae3f6d3a9f3b5bf2c2fb1012ced3665
luiskcs89 <luiskcs89@gmail.com>
2018-04-24 15:10:53 -0600

    feat(connections): add get task tree from teamwork function

--- functions/src/services/teamwork.service.ts
+++ functions/src/services/teamwork.service.ts
@@ -105,6 +105,86 @@ export class TeamworkService {
     }
   }
 
+  async getTasks(token: string, tasklistId: string) {
+    try {
+      const tasksData = await this.getTasksData(token, tasklistId, 1);
+      let tasks = tasksData.tasks;
+
+      if (tasksData.pages > 1) {
+        let taskPromises = [];
+        for (var i = 2; i <= tasksData.pages; i++) {
+          taskPromises.push(this.getTasksData(token, tasklistId, 1));
+        };
+        const moreTasksData = await Promise.all(taskPromises);
+        moreTasksData.map(
+          t => {
+            tasks = tasks.concat(t.tasks);
+          }
+        );
+      }
+
+      const tasksTree = tasks.filter(t => t.parent == '');
+      this.fillChildTasks(tasksTree, tasks);
+      
+      return tasksTree;
+      
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private fillChildTasks(tasks, allTasks) {
+    tasks.map(
+      t => {
+        t.childTasks = allTasks.filter(c => c.parent == t.id);
+        if(t.childTasks.length > 0) {
+          this.fillChildTasks(t.childTasks, allTasks);
+        } 
+      }
+    );
+  }
+
+  private async getTasksData(token: string, tasklistId: string, page: number) {
+    let authData;
+    try {
+      authData = await this.validateToken(token);
+      const uri = `${authData.baseUrl}/tasklists/${tasklistId}/tasks.json?page=${page}`;
+      const headers = this.getReqHeaders(token);
+
+      const options: request.OptionsWithUri = {
+        uri,
+        method: 'GET',
+        headers,
+        json: true,
+        resolveWithFullResponse: true,
+      };
+
+      return request(options)
+        .then(response => {
+          const tasks = response.body['todo-items'];
+          const responseHeaders = response.headers;
+
+          return {
+            page: responseHeaders['x-page'],
+            pages: responseHeaders['x-pages'],
+            tasks: tasks.map(
+              t => {
+                return {
+                  id: t.id,
+                  name: t.content,
+                  description: t.description,
+                  parent: t.parentTaskId
+                };
+              }
+            )
+          };
+        })
+        .catch(error => { throw new Error('Unable to get tasks from Teamwork. Please try again later.') });
+    } catch (error) {
+      throw error;
+    }
+  }
+
   private getReqHeaders(token: string) {
     const encodedToken = new Buffer(`${token}:X`).toString('base64');
     return {
