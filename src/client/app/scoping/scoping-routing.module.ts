970f970b331a189f19a3ddc23e66d9cfdff92ba4
Khaled Shaaban <kshaaban@outlook.com>
2018-04-26 18:40:33 -0400

    feat: generate session results container and routes

--- src/client/app/scoping/scoping-routing.module.ts
+++ src/client/app/scoping/scoping-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SessionScopingComponent } from '@app/scoping/containers/session-scoping/session-scoping.component';
+import { SessionResultsComponent } from '@app/scoping/containers/session-results/session-results.component';
 
 const routes: Routes = [
   {
     path: ':sessionId',
     component: SessionScopingComponent,
+  },
+  {
+    path: ':sessionId/results',
+    component: SessionResultsComponent,
   },
 ];
 
