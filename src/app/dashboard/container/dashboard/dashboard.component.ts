ad91d4fbee899635584239cb0be751ce1596f0fe
Khaled Shaaban <kshaaban@outlook.com>
2018-04-26 17:59:18 -0400

    fix: merge conflict in commit 8e78a0c

---
+++ src/app/dashboard/container/dashboard/dashboard.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { TakeUntilDestroy, untilDestroyed } from 'ngx-take-until-destroy';
+
+import { map, filter, withLatestFrom, take } from 'rxjs/operators';
+
+import { AuthFacade } from '@app/authentication/state/auth.facade';
+import { AuthUiState } from '@app/authentication/state/auth.reducer';
+import { RouterFacade } from '@app/state/router.facade';
+
+@TakeUntilDestroy()
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+})
+export class DashboardComponent implements OnDestroy {
+  user$ = this.authFacade.user$;
+
+  private logoutRedirect$ = this.authFacade.uiState$.pipe(
+    untilDestroyed(this),
+    filter(uiState => uiState === AuthUiState.NOT_AUTHENTICATED),
+    withLatestFrom(this.routerFacade.queryParams$)
+  );
+
+  constructor(
+    private authFacade: AuthFacade,
+    private routerFacade: RouterFacade
+  ) {}
+
+  ngOnDestroy() {}
+
+  logout() {
+    this.authFacade.logout();
+
+    this.logoutRedirect$.pipe(take(1)).subscribe(() => {
+      this.routerFacade.navigate({ path: ['/login'] });
+    });
+  }
+}
