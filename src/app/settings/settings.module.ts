ca15518f3bd1989b2471dcb8c656b57a58b37635
Khaled Shaaban <kshaaban@outlook.com>
2018-04-26 18:19:18 -0400

    chore: remove no-longer needed manage-connections page & route

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { SettingsRoutingModule } from './settings-routing.module';
-import { SettingsComponent } from './containers/settings/settings.component';
-import { ManageConnectionsComponent } from './containers/manage-connections/manage-connections.component';
+import { SettingsRoutingModule } from '@app/settings/settings-routing.module';
+import { SettingsComponent } from '@app/settings/containers/settings/settings.component';
 
 @NgModule({
   imports: [CommonModule, SettingsRoutingModule],
-  declarations: [SettingsComponent, ManageConnectionsComponent],
+  declarations: [SettingsComponent],
 })
 export class SettingsModule {}
