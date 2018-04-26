f0c2ce607d577d36f23e1b835384463bbd6187b8
Khaled Shaaban <kshaaban@outlook.com>
2018-04-26 18:39:47 -0400

    feat: generate session scoping component & routes

--- src/client/app/scoping/scoping.module.ts
+++ src/client/app/scoping/scoping.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ScopingRoutingModule } from '@app/scoping/scoping-routing.module';
+import { SessionScopingComponent } from './containers/session-scoping/session-scoping.component';
 
 @NgModule({
   imports: [CommonModule, ScopingRoutingModule],
-  declarations: [],
+  declarations: [SessionScopingComponent],
 })
 export class ScopingModule {}
