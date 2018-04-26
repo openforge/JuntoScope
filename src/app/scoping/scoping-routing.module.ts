45726dc26cc71ab61b2edeb7ac9717bfe8fd6843
Khaled Shaaban <kshaaban@outlook.com>
2018-04-26 18:39:47 -0400

    feat: generate session scoping component & routes

--- src/app/scoping/scoping-routing.module.ts
+++ src/app/scoping/scoping-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SessionScopingComponent } from '@app/scoping/containers/session-scoping/session-scoping.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: ':sessionId',
+    component: SessionScopingComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
