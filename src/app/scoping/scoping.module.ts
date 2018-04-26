da9bf9e3ea467e9da0945eab89e95331f3817685
Khaled Shaaban <kshaaban@outlook.com>
2018-04-26 18:40:33 -0400

    feat: generate session results container and routes

--- src/app/scoping/scoping.module.ts
+++ src/app/scoping/scoping.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ScopingRoutingModule } from '@app/scoping/scoping-routing.module';
 import { SessionScopingComponent } from './containers/session-scoping/session-scoping.component';
+import { SessionResultsComponent } from './containers/session-results/session-results.component';
 
 @NgModule({
   imports: [CommonModule, ScopingRoutingModule],
-  declarations: [SessionScopingComponent],
+  declarations: [SessionScopingComponent, SessionResultsComponent],
 })
 export class ScopingModule {}
