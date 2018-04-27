2281f9e2f645af9d967d4af00c752ff9f1ef545b
luiskcs89 <luiskcs89@gmail.com>
2018-04-27 11:33:23 -0600

    feat(connections): use ionic components on create session container

--- src/app/connections/containers/create-session/create-session.component.ts
+++ src/app/connections/containers/create-session/create-session.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { RouterFacade } from '@app/state/router.facade';
 
 @Component({
   selector: 'app-create-session',
@@ -6,7 +8,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-session.component.scss'],
 })
 export class CreateSessionComponent implements OnInit {
-  constructor() {}
+  sessionForm: FormGroup;
+  step: number;
 
-  ngOnInit() {}
+  constructor(private fb: FormBuilder, public routerFacade: RouterFacade) {}
+
+  ngOnInit() {
+    this.step = 0;
+    this.createForm();
+  }
+
+  goToTaskLists() {
+    if (this.sessionForm.get('project').valid) {
+      this.step = 1;
+    } else {
+      this.sessionForm.get('project').markAsDirty();
+    }
+  }
+
+  goToScopeLink() {
+    if (this.sessionForm.get('tasklist').valid) {
+      this.step = 2;
+    } else {
+      this.sessionForm.get('tasklist').markAsDirty();
+    }
+  }
+
+  goBack() {
+    if (this.step > 0) {
+      this.step--;
+    } else {
+      this.routerFacade.back();
+    }
+  }
+
+  goToScoping(sessionId) {
+    this.routerFacade.navigate({ path: [`/scoping/${sessionId}`] });
+  }
+
+  createForm() {
+    this.sessionForm = this.fb.group({
+      project: ['', Validators.required],
+      tasklist: ['', Validators.required],
+    });
+  }
 }
