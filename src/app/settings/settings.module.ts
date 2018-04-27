179b0d458b1b8f64833feca7f9ef958f0e132e33
luiskcs89 <luiskcs89@gmail.com>
2018-04-27 14:18:47 -0600

    feat(settings): import IonicModule and use ionic components on settings container

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { SettingsRoutingModule } from '@app/settings/settings-routing.module';
 import { SettingsComponent } from '@app/settings/containers/settings/settings.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
-  imports: [CommonModule, SettingsRoutingModule],
+  imports: [CommonModule, SettingsRoutingModule, IonicModule],
   declarations: [SettingsComponent],
 })
 export class SettingsModule {}
