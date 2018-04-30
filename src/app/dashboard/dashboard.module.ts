3b3937e36534df1d0826c7a078f82f407f309273
Khaled Shaaban <kshaaban@outlook.com>
2018-04-30 15:43:10 -0400

    feat(dashboard): create join session component

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,8 +4,10 @@ import { SharedModule } from '@app/shared/shared.module';
 import { DashboardRoutingModule } from '@app/dashboard/dashboard-routing.module';
 import { DashboardComponent } from '@app/dashboard/container/dashboard/dashboard.component';
 
+import { JoinSessionComponent } from '@app/dashboard/components/join-session/join-session.component';
+
 @NgModule({
   imports: [SharedModule, DashboardRoutingModule],
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, JoinSessionComponent],
 })
 export class DashboardModule {}
