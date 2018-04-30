54e481297745dadbb8e012f93e98ff2e3fb822bf
Khaled Shaaban <kshaaban@outlook.com>
2018-04-30 11:36:44 -0400

    refactor: create shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,20 +12,19 @@ import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { environment } from '@env/environment';
 import { AppRoutingModule } from '@app/app-routing.module';
-import { reducers, metaReducers, initialState } from '@app/state/app.reducer';
-import { AppComponent } from '@app/app.component';
+import { SharedModule } from '@app/shared/shared.module';
 import { AuthenticationModule } from '@app/authentication/authentication.module';
+import { ConnectionsModule } from '@app/connections/connections.module';
+import { AppComponent } from '@app/app.component';
 import { NotFoundComponent } from '@app/not-found.component';
 import { AuthGuard } from '@app/auth.guard';
 import { UnAuthGuard } from '@app/un-auth.guard';
 import { AppFacade } from '@app/state/app.facade';
-import { ApiInterceptor } from '@app/api.interceptor';
-import { ConnectionsModule } from '@app/connections/connections.module';
+import { reducers, metaReducers, initialState } from '@app/state/app.reducer';
 
 @NgModule({
   declarations: [AppComponent, NotFoundComponent],
   imports: [
-    HttpClientModule,
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule.forRoot(),
@@ -35,19 +33,11 @@ import { ConnectionsModule } from '@app/connections/connections.module';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFirestoreModule,
+    SharedModule.forRoot(),
     AuthenticationModule.forRoot(),
     ConnectionsModule.forRoot(),
   ],
-  providers: [
-    AppFacade,
-    AuthGuard,
-    UnAuthGuard,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: ApiInterceptor,
-      multi: true,
-    },
-  ],
+  providers: [AppFacade, AuthGuard, UnAuthGuard],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,26 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { ApiInterceptor } from '@app/shared/api.interceptor';
+
+@NgModule({
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule, IonicModule],
+  exports: [CommonModule, ReactiveFormsModule, HttpClientModule, IonicModule],
+})
+export class SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: [
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: ApiInterceptor,
+          multi: true,
+        },
+      ],
+    };
+  }
+}
