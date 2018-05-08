f63e09441bfc6eabc425e335b2714421ba6480e8
daftclaud <cdsmileyone@gmail.com>
2018-05-08 13:27:21 -0400

    feat(Dashboard): Adds create-session component.
    Creates components and containers and sets up prelimirary routing.

---
+++ src/app/dashboard/components/task-list/task-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.scss'],
+})
+export class TaskListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from '@app/dashboard/container/dashboard/dashboard.component';
+import { SelectProjectComponent } from '@app/dashboard/container/select-project/select-project.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
+  },
+  {
+    path: 'create-session/select-project',
+    component: SelectProjectComponent,
   },
 ];
 
