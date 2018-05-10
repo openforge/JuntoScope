e452fb8673967205ab4d03fdcb39812955765f54
Fdom92 <fernando.olmo92@gmail.com>
2018-05-10 16:14:34 -0400

    fix: merge conflict in commit fe55399

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,11 +6,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { ApiInterceptor } from '@app/shared/api.interceptor';
 import { PopupService } from '@app/shared/popup.service';
-import {
-  ObjectKeysPipe,
-  ObjectValuesPipe,
-  ObjectKeyValuePipe,
-} from './pipes/object-iterators.pipe';
+import { InfoModalComponent } from '@app/shared/components/info-modal/info-modal.component';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule, HttpClientModule, IonicModule],
@@ -19,11 +15,10 @@ import {
     ReactiveFormsModule,
     HttpClientModule,
     IonicModule,
-    ObjectKeysPipe,
-    ObjectValuesPipe,
-    ObjectKeyValuePipe,
+    InfoModalComponent,
   ],
-  declarations: [ObjectKeysPipe, ObjectValuesPipe, ObjectKeyValuePipe],
+  declarations: [InfoModalComponent],
+  entryComponents: [InfoModalComponent],
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders {
