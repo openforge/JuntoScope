89547decb065af6b0eb0160fe8258b40df28a2ce
daftclaud <cdsmileyone@gmail.com>
2018-05-10 17:13:07 -0400

    removes task-item

--- src/app/connections/connections.module.ts
+++ src/app/connections/connections.module.ts
@@ -13,7 +13,6 @@ import { ConnectionFacade } from '@app/connections/state/connection.facade';
 import { ConnectionService } from '@app/connections/services/connection.service';
 import { ProjectListComponent } from './components/project-list/project-list.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
-import { TaskItemComponent } from './components/task-item/task-item.component';
 import { ProjectItemComponent } from './components/project-item/project-item.component';
 import { SelectProjectComponent } from './containers/select-project/select-project.component';
 import { SelectTaskListComponent } from './containers/select-task-list/select-task-list.component';
@@ -32,7 +31,6 @@ import { VerifyModalComponent } from '@app/connections/components/verify-modal/v
     ConnectionDetailsComponent,
     ProjectListComponent,
     TaskListComponent,
-    TaskItemComponent,
     ProjectItemComponent,
     SelectProjectComponent,
     SelectTaskListComponent,
