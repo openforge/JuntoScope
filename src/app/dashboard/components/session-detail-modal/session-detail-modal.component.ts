215a45b08bcc770634725c03f2137f43105330c4
Fdom92 <fernando.olmo92@gmail.com>
2018-05-11 14:35:08 -0400

    fix: merge conflicts in commit f9e5887

--- src/app/dashboard/components/session-detail-modal/session-detail-modal.component.ts
+++ src/app/dashboard/components/session-detail-modal/session-detail-modal.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit, Input } from '@angular/core';
 import { NavParams } from '@ionic/angular';
 import { PopupService } from '@app/shared/popup.service';
 import { InfoModalComponent } from '@app/shared/components/info-modal/info-modal.component';
+import { AppState } from '@app/state/app.reducer';
+import { Store } from '@ngrx/store';
+import { RefreshAccessCodeAction } from '@app/dashboard/state/dashboard.actions';
+
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-session-detail-modal',
@@ -11,30 +16,39 @@ import { InfoModalComponent } from '@app/shared/components/info-modal/info-modal
 export class SessionDetailModalComponent implements OnInit {
   accountData;
   isModerator: boolean;
+  expirationDate;
 
-  constructor(private popupSvc: PopupService, private params: NavParams) {}
+  constructor(
+    private popupSvc: PopupService,
+    private params: NavParams,
+    private store: Store<AppState>
+  ) {}
 
   ngOnInit() {
     this.accountData = this.params.data.accountData;
     this.isModerator =
       this.params.data.accountData.userType === 'Session Moderator';
+    const now = moment();
+    this.expirationDate = now.to(this.accountData.item.expirationDate);
   }
 
-  dismiss() {
+  refreshCode() {
     this.popupSvc.closeModal();
+    this.store.dispatch(
+      new RefreshAccessCodeAction(this.accountData.item.sessionCode)
+    );
   }
 
   deleteSession() {
     this.popupSvc.closeModal();
-    this.popupSvc.openModal(
-      {
-        component: InfoModalComponent,
-        componentProps: {
-          'title': 'Are you sure?',
-          'text': 'If you delete the session there is no come back.',
-          'label': 'Delete',
-          'callback': () => console.log('accepted to delete session')
-        }
-      });
+    this.popupSvc.openModal({
+      component: InfoModalComponent,
+      componentProps: {
+        title: 'Are you sure?',
+        text: 'If you delete the session there is no come back.',
+        label: 'Delete',
+        callback: () => console.log('accepted to delete session'),
+      },
+    });
   }
 }
