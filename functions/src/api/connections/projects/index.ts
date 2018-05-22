8fe5a9007e6e796cccb347ac4e5e312abafb5dbb
mohammad-alfatih <alfaitha@gmail.com>
2018-05-22 14:19:22 -0400

    chore: add skeleton endpoints

--- functions/src/api/connections/projects/index.ts
+++ functions/src/api/connections/projects/index.ts
@@ -1,7 +1,10 @@
 import * as express from 'express';
 
+import { getProjects } from './get-projects';
 import { sessionsRouter } from './sessions';
 
 export const projectsRouter = express.Router({ mergeParams: true });
+
+projectsRouter.get('/', getProjects);
 
 projectsRouter.use('/:projectId/sessions', sessionsRouter);
