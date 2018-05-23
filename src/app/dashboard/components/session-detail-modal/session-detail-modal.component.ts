d4acd3dace87530a8b8830c5a12599eafd1925de
mohammad-alfatih <alfaitha@gmail.com>
2018-05-23 15:26:38 -0400

    fix: removed double imports in session-detail-modal.component.ts

--- src/app/dashboard/components/session-detail-modal/session-detail-modal.component.ts
+++ src/app/dashboard/components/session-detail-modal/session-detail-modal.component.ts
@@ -7,8 +7,6 @@ import { Store } from '@ngrx/store';
 import { RefreshAccessCodeAction } from '@app/dashboard/state/dashboard.actions';
 
 import * as moment from 'moment';
-import { Store } from '@ngrx/store';
-import { AppState } from '@app/state/app.reducer';
 import { DeleteSessionAction } from '@app/dashboard/state/dashboard.actions';
 
 @Component({
