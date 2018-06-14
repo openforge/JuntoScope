6774f7edfcc5e8a13d324108f693915c5477ffde
mohammad-alfatih <alfaitha@gmail.com>
2018-06-14 00:55:01 -0400

    chore: complete app.component.spec and successfully pass unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,15 @@
+import { AuthFacade } from './authentication/state/auth.facade';
+import { Store } from '@ngrx/store';
+import { StoreModule } from '@ngrx/store';
+import { reducers, initialState } from '../app/state/app.reducer';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Actions } from '@ngrx/effects';
+import { StoreMock } from './mocks/ngrx/store.mock';
+import { AngularFireAuthMock } from './mocks/angularfire/angularfireauth.mock';
+import { AuthService } from './authentication/services/auth.service';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
 
@@ -9,13 +19,31 @@ describe('AppComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [
+        RouterTestingModule,
+        StoreModule.forRoot(reducers, {
+          initialState: { router: initialState.router },
+        }),
+      ],
+      providers: [
+        Actions,
+        AuthFacade,
+        AuthService,
+        {
+          provide: AngularFireAuth,
+          useValue: AngularFireAuthMock,
+        },
+        {
+          provide: Store,
+          useValue: new StoreMock(initialState),
+        },
+      ],
       declarations: [AppComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   }));
 
   it('should create the app', async(() => {
