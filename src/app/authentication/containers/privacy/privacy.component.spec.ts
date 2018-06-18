f48b897c53358f75ce56768c6fe0a83f37736f07
mohammad-alfatih <alfaitha@gmail.com>
2018-06-17 22:34:49 -0400

    fix: failing tests in privacy.component.spec fixed

--- src/app/authentication/containers/privacy/privacy.component.spec.ts
+++ src/app/authentication/containers/privacy/privacy.component.spec.ts
@@ -2,20 +2,44 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PrivacyComponent } from './privacy.component';
 
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { StoreModule, Store } from '@ngrx/store';
+import { Actions } from '@ngrx/effects';
+import { reducers, initialState } from '../../../../app/state/app.reducer';
+import { RouterFacade } from '../../../state/router.facade';
+
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { StoreMock } from './../../../mocks/ngrx/store.mock';
+
 describe('PrivacyComponent', () => {
   let component: PrivacyComponent;
   let fixture: ComponentFixture<PrivacyComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        StoreModule.forRoot(reducers, {
+          initialState: { router: initialState.router },
+        }),
+      ],
+      providers: [
+        Actions,
+        RouterFacade,
+        {
+          provide: Store,
+          useValue: new StoreMock(initialState),
+        },
+      ],
       declarations: [PrivacyComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PrivacyComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
