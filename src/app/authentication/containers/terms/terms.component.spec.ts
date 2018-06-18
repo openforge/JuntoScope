e7bdab0bf5e973e02fa0792bdf396d661b7615f4
mohammad-alfatih <alfaitha@gmail.com>
2018-06-17 22:35:09 -0400

    fix: failing tests in terms.component.spec fixed

--- src/app/authentication/containers/terms/terms.component.spec.ts
+++ src/app/authentication/containers/terms/terms.component.spec.ts
@@ -1,4 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterFacade } from '../../../state/router.facade';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { StoreModule, Store } from '@ngrx/store';
+import { Actions } from '@ngrx/effects';
+import { StoreMock } from './../../../mocks/ngrx/store.mock';
+import { initialState, reducers } from '../../../state/app.reducer';
+
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { TermsComponent } from './terms.component';
 
@@ -8,7 +17,22 @@ describe('TermsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        StoreModule.forRoot(reducers, {
+          initialState: { router: initialState.router },
+        }),
+        RouterTestingModule,
+      ],
+      providers: [
+        Actions,
+        RouterFacade,
+        {
+          provide: Store,
+          useValue: new StoreMock(initialState),
+        },
+      ],
       declarations: [TermsComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
 
