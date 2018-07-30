162061ae202776b2546445ed45520aba48e04674
Paulina Gallo <paulinaggallo@gmail.com>
2018-07-30 15:08:21 -0400

    refactor(settings): refactor styles, layout, and functionality

--- src/features/settings/pages/settings/settings.module.ts
+++ src/features/settings/pages/settings/settings.module.ts
@@ -3,11 +3,9 @@ import { IonicPageModule } from "ionic-angular";
 import { SettingsPage } from "./settings";
 import { SettingsModule } from "../../settings.module";
 
-import { ConnectionsModule } from "../../../connections/connections.module";
-
 @NgModule({
   declarations: [SettingsPage],
-  imports: [IonicPageModule.forChild(SettingsPage), SettingsModule, ConnectionsModule],
+  imports: [IonicPageModule.forChild(SettingsPage), SettingsModule],
   exports: [SettingsPage]
 })
 export class SettingsPageModule {}
