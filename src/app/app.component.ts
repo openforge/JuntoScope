996df2e9d41ab7d6c086ab3457208a8eca99b3b6
daftclaud <cdsmileyone@gmail.com>
2018-07-30 15:28:10 -0400

    fix(auth): checks authstate on initialization
    #44

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Platform } from "ionic-angular";
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
-
-// import { AuthFacade } from '@app/authentication/state/auth.facade';
+import { AuthEffects } from "../features/authentication/store/auth.effects";
 
 @Component({
   templateUrl: "app.html"
@@ -14,13 +13,16 @@ export class JuntoScopeComponent {
   constructor(
     platform: Platform,
     statusBar: StatusBar,
-    splashScreen: SplashScreen
+    splashScreen: SplashScreen,
+    authFacade: AuthEffects
   ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      authFacade.checkAuth();
     });
   }
 }
