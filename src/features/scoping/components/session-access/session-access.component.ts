7911da03be73e4d5d2a49f0e8bf0267d46c1f30e
Fdom92 <fer.olmo92@gmail.com>
2018-08-10 13:12:35 +0200

    fix(): fix ticket 54

--- src/features/scoping/components/session-access/session-access.component.ts
+++ src/features/scoping/components/session-access/session-access.component.ts
@@ -1,23 +1,41 @@
-import { Component, OnInit, Output, Input, EventEmitter } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Output,
+  Input,
+  EventEmitter,
+  OnDestroy
+} from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { ScopingFacade } from "../../store/scoping.facade";
 
 import { SessionValidation } from "../../../../models/scoping-session";
+import { Store } from "@ngrx/store";
+import { AppState } from "../../../../store/app.reducer";
+import { CleanErrorAction } from "../../store/scoping.actions";
 @Component({
   selector: "app-session-access",
   templateUrl: "./session-access.component.html"
 })
-export class SessionAccessComponent implements OnInit {
+export class SessionAccessComponent implements OnInit, OnDestroy {
   accessForm: FormGroup;
   error$ = this.scopingFacade.error$;
 
   @Input() sessionLink: string;
   @Output() access = new EventEmitter<SessionValidation>();
 
-  constructor(private fb: FormBuilder, private scopingFacade: ScopingFacade) {}
+  constructor(
+    private store: Store<AppState>,
+    private fb: FormBuilder,
+    private scopingFacade: ScopingFacade
+  ) {}
 
   ngOnInit() {
     this.createForm();
+  }
+
+  ngOnDestroy() {
+    this.store.dispatch(new CleanErrorAction());
   }
 
   continue() {
