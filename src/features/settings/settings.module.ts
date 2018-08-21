8f2e53673bb9091cb1db159742c4552b3af3388b
Fernando del olmo <fer.olmo92@gmail.com>
2018-08-21 13:02:47 +0200

    feat(): add faqs from firestore

--- src/features/settings/settings.module.ts
+++ src/features/settings/settings.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from "@angular/core";
 import { SharedModule } from "../../shared/shared.module";
+import { SettingsService } from "./service/settings.service";
+import { ModuleWithProviders } from "@angular/compiler/src/core";
 
 @NgModule({
   imports: [SharedModule],
@@ -8,4 +10,11 @@ import { SharedModule } from "../../shared/shared.module";
   entryComponents: [],
   providers: []
 })
-export class SettingsModule {}
+export class SettingsModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SettingsModule,
+      providers: [SettingsService]
+    };
+  }
+}
