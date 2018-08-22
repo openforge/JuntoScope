d730600ba3e1befe49d6b6f8fe1a0e0319af4f22
Fernando del olmo <fer.olmo92@gmail.com>
2018-08-22 18:32:02 +0200

    fix(): set prod env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 import {
-  FIREBASE_DEV_CONFIG,
-  FIREBASE_DEV_FUNCTIONS,
-  GOOGLE_WEB_CLIENT_ID_DEV
+  FIREBASE_PROD_FUNCTIONS,
+  FIREBASE_PROD_CONFIG,
+  GOOGLE_WEB_CLIENT_ID_PROD
 } from "../config/config";
 
 export const environment = {
-  production: false,
-  apiBaseUrl: FIREBASE_DEV_FUNCTIONS,
-  firebase: FIREBASE_DEV_CONFIG,
-  webClientId: GOOGLE_WEB_CLIENT_ID_DEV
+  production: true,
+  apiBaseUrl: FIREBASE_PROD_FUNCTIONS,
+  firebase: FIREBASE_PROD_CONFIG,
+  webClientId: GOOGLE_WEB_CLIENT_ID_PROD
 };
