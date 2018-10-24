6556bac062224d834695f5354996b8d8b30fe534
Sam H <sam@Sams-MacBook-Pro.local>
2018-10-24 13:35:33 -0400

    test: add files to protractor.conf.js

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,5 @@
 exports.config = {
   framework: "jasmine",
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ["./src/**/*.spec.ts", "todo-spec.js"]
+  specs: ["./src/**/*.spec.ts", "./src/protractor-setup/todo-spec.js"]
 };
