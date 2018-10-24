342462071f2e85c51f9851d29820d4f21c0800ff
Sam H <sam@Sams-MacBook-Pro.local>
2018-10-24 14:06:08 -0400

    test(edit tsconfig.spec.json, protractor.conf.js): edit tsconfig.spec.json, protractor.conf.js
    edit the protractor config to have a beforeLaunch property that sets up the ts-node and tsconfig
    files so that we can stop getting the unexpected token { error

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,14 @@
 exports.config = {
   framework: "jasmine",
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ["src/**/**/*.spec.ts", "src/**/**/*.spec.js", "src/**/*-spec.js"]
+  specs: ["src/**/**/*.spec.ts", "src/**/**/*.spec.js", "src/**/*-spec.js"],
+
+  beforeLaunch: () => {
+    require("ts-node").register({
+      project: "src/tsconfig.spec.json"
+    });
+  }
+  // onPrepare() {
+  //   jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+  // }
 };
