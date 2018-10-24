0ae328d3e58cc4aebe93551bce6468b87ea5293b
Sam H <sam@Sams-MacBook-Pro.local>
2018-10-24 15:19:17 -0400

    test: uninstall jest, try to debug ionic serve errors related to @types/node module
    the @types/node and @types/jasmine modules are required to get the program to recognize "describe"
    and "it" functions in e2e test, but they interfere with the proper running of the app with ionic
    serve

---
+++ e2e/e2e.spec.ts
@@ -0,0 +1,7 @@
+import { browser, element, by, ElementFinder } from "protractor";
+
+describe("Example E2E Test", () => {
+  beforeEach(() => {
+    browser.get("");
+  });
+});
--- protractor.conf.js
+++ protractor.conf.js
@@ -1,7 +1,9 @@
+const { SpecReporter } = require('jasmine-spec-reporter');
+
 exports.config = {
   framework: "jasmine",
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ["src/**/**/*.spec.ts", "src/**/**/*.spec.js", "src/**/*-spec.js"],
+  specs: ["src/**/**/*.spec.ts", "src/**/**/*.spec.js", "src/**/*-spec.js", "e2e/*.spec.ts"],
 
   beforeLaunch: () => {
     require("ts-node").register({
@@ -9,10 +11,11 @@ exports.config = {
     });
 
     require("zone.js/dist/zone-node");
-    require("zone.js/dist/async-test.js");
-    require("zone.js/dist/proxy.js");
+    // require("zone.js/dist/async-test.js");
+    // require("zone.js/dist/proxy.js");
+  },
+
+  onPrepare: () => {
+    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
-  // onPrepare() {
-  //   jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  // }
 };
