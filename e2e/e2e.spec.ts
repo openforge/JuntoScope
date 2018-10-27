18f51061281a4e6b357091bcf9a78f062ec2f008
Sam H <sam@Sams-MacBook-Pro.local>
2018-10-27 13:46:23 -0400

    test: attempt more ways to test elements on login page

--- e2e/e2e.spec.ts
+++ e2e/e2e.spec.ts
@@ -4,26 +4,17 @@ describe("Example E2E Test", () => {
   beforeEach(() => {});
 
   it("the login page is displayed by default", () => {
-    // browser.get("http://localhost:8100/#/LoginPage");
-
-    // var introText = element(by.css(".container p")).getText();
-
-    // var textContent = "First, we need a way of identifying your participation.";
-
-    // expect(introText).toContain(textContent);
-
     browser.get("http://localhost:8100/#/LoginPage");
 
     // var container = element(by.css(".container"));
-    var agreeCheckboxLabel = element(
-      by.xpath(
-        "/html/body/ion-app/ng-component/ion-nav/app-login/ion-content/div[2]/div/button[3]/span"
-      )
-    );
+    // var agreeCheckboxLabel = element(by.xpath("/html/body/ion-app/ng-component/ion-nav/app-login/ion-content/div[2]/div/button[3]/span"));
 
-    var clickAgree = element(by.css('[name="agree"]'));
+    // var clickAgree = element(by.css('[name="agree"]'));
+    //clickAgree.click();
 
-    clickAgree.click();
+    var twitterButton = element(by.partialButtonText("Twitter"));
+
+    // expect(twitterButton).toBeFalsy();
 
     // expect(container).toBeTruthy();
     // expect(agreeCheckboxLabel.getText()).toEqual('Twitter');
