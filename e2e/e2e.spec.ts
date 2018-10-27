12db7c7d1d763175686cabcf707bbae7b37e516d
Sam H <sam@Sams-MacBook-Pro.local>
2018-10-27 14:30:27 -0400

    test: try to click "I Agree" checkbox during test, still timeout error

--- e2e/e2e.spec.ts
+++ e2e/e2e.spec.ts
@@ -2,11 +2,11 @@ import { browser, element, by, ElementFinder } from "protractor";
 
 describe("Example E2E Test", () => {
   // So far, this test has a timeout error any time I try almost any expect() statement besides "toBeTruthy()"
+  // Also timeout error when I try something like clicking the checkbox for "I Agree". Might not be getting the element properly
   it("the login page is displayed by default", () => {
     browser.get("http://localhost:8100/#/LoginPage");
 
-    var greeting = element(by.css(".container p"));
-
-    expect(greeting.getAttribute("innerHTML")).toContain("First");
+    var checkbox = element(by.tagName("ion-checkbox"));
+    checkbox.click();
   });
 });
