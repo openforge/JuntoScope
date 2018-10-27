3e25c0902cb5b0fd35bed8d4c157c50dc9efe568
Sam H <sam@Sams-MacBook-Pro.local>
2018-10-27 14:17:32 -0400

    test: try to make short protractor test with promises, still timeout error

--- e2e/e2e.spec.ts
+++ e2e/e2e.spec.ts
@@ -1,19 +1,16 @@
 import { browser, element, by, ElementFinder } from "protractor";
 
 describe("Example E2E Test", () => {
-  beforeEach(() => {
-    browser.get("http://localhost:8100/#/LoginPage");
-  });
-
-  // So far, this test times out any time I try almost any expect() statement besides "toBeTruthy()"
+  // So far, this test has a timeout error any time I try almost any expect() statement besides "toBeTruthy()"
   it("the login page is displayed by default", () => {
+    browser.get("http://localhost:8100/#/LoginPage");
+
     var greeting = element(by.css("div p"));
 
-    expect(greeting.getText()).toContain("First");
+    greeting.getText().then(function(value) {
+      console.log(value);
 
-    // expect(twitterButton).toBeFalsy();
-
-    // expect(container).toBeTruthy();
-    // expect(agreeCheckboxLabel.getText()).toEqual('Twitter');
+      expect(value).toContain("First");
+    });
   });
 });
--- src/protractor-setup/example-spec.js
+++ src/protractor-setup/example-spec.js
@@ -1,17 +1,17 @@
-// example-spec.js
-describe("angularjs homepage", function() {
-  it("should exist", function() {
-    browser.get("http://www.angularjs.org");
-    element(by.model("yourName")).sendKeys("Julie");
-    var greeting = element(by.binding("yourName"));
-    expect(greeting.getText()).toEqual("Hello Julie!");
+// // example-spec.js
+// describe("angularjs homepage", function() {
+//   it("should exist", function() {
+//     browser.get("http://www.angularjs.org");
+//     element(by.model("yourName")).sendKeys("Julie");
+//     var greeting = element(by.binding("yourName"));
+//     expect(greeting.getText()).toEqual("Hello Julie!");
 
-    // browser.get("http://localhost:8100/#/LoginPage");
+//     // browser.get("http://localhost:8100/#/LoginPage");
 
-    // var container = element(by.css(".jedi"));
+//     // var container = element(by.css(".jedi"));
 
-    // expect(container).toBeTruthy();
+//     // expect(container).toBeTruthy();
 
-    // browser.pause();
-  });
-});
+//     // browser.pause();
+//   });
+// });
