0940d0dcc6cba9cacbcc992345266d6ec8bf191a
Sam H <sam@Sams-MacBook-Pro.local>
2018-10-29 13:58:30 -0400

    test: fix protractor testing timeout error with browser.ignoreSynchronization

---
+++ e2e/dashboard.spec.ts
@@ -0,0 +1,14 @@
+// import { browser, element, by, ElementFinder } from "protractor";
+
+// describe("Dashboard stuff", () => {
+
+//   beforeEach(function() {
+//     browser.ignoreSynchronization = true;
+//   })
+
+//   it("should do dashboard stuff", () => {
+//     browser.get("http://localhost:8100/#/DashboardPage");
+
+//     element(by.css(".searchbar-input")).sendKeys("testing input");
+//   });
+// });
--- e2e/e2e.spec.ts
+++ e2e/e2e.spec.ts
@@ -1,14 +1,18 @@
 import { browser, element, by, ElementFinder } from "protractor";
 
 describe("Example E2E Test", () => {
-  // So far, this test has a timeout error any time I try almost any expect() statement besides "toBeTruthy()"
-  // Also timeout error when I try something like clicking the checkbox for "I Agree". Might not be getting the element properly
+  beforeEach(function() {
+    browser.ignoreSynchronization = true;
+  });
+
   it("the login page is displayed by default", () => {
     browser.get("http://localhost:8100/#/LoginPage");
 
-    var checkbox = element(by.css('[name="agree"]'));
+    var checkbox = element(by.css("ion-checkbox"));
 
-    // checkbox.click();
+    checkbox.click();
+
+    browser.pause();
 
     // expect(checkbox).toBeTruthy();
   });
