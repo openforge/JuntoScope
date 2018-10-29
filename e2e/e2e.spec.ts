e168b934f1871b261080440ea5807e03b98851ed
Sam H <sam@Sams-MacBook-Pro.local>
2018-10-29 16:25:47 -0400

    test: fix timeout bug, add more to e2e test

--- e2e/e2e.spec.ts
+++ e2e/e2e.spec.ts
@@ -1,19 +1,47 @@
 import { browser, element, by, ElementFinder } from "protractor";
 
 describe("Example E2E Test", () => {
-  beforeEach(function() {
-    browser.ignoreSynchronization = true;
+  beforeEach(function() {});
+
+  beforeAll(function() {
+    browser.get("http://localhost:8100/#/LoginPage");
+    // browser.ignoreSynchronization = true;
+    browser.waitForAngularEnabled(false);
   });
 
-  it("the login page is displayed by default", () => {
-    browser.get("http://localhost:8100/#/LoginPage");
+  it("should not be able to login before the agree checkbox has been clicked", () => {
+    var loginButtons = element.all(by.css(".container > button"));
+    var loginButtonsCount;
 
-    var checkbox = element(by.css("ion-checkbox"));
+    loginButtons.count().then(value => {
+      loginButtonsCount = value;
+    });
 
+    // for (let i=0; i < loginButtonsCount; i++) {
+    //   expect(loginButtons.get(i).isEnabled()).toBe(true);
+    // }
+
+    expect(loginButtons.get(0).isEnabled()).toBe(false);
+    expect(loginButtons.get(1).isEnabled()).toBe(false);
+    expect(loginButtons.get(2).isEnabled()).toBe(false);
+  });
+
+  it("should click the Agree checkbox", () => {
+    var checkbox = element(by.css("[role='checkbox']"));
     checkbox.click();
 
-    browser.pause();
+    browser.sleep(1500);
 
-    // expect(checkbox).toBeTruthy();
+    expect(checkbox.getAttribute("aria-checked")).toEqual("true");
+  });
+
+  it("should click the Twitter button", () => {
+    var twitterButton = element(by.css(".container button:nth-of-type(3)"));
+
+    twitterButton.click();
+
+    browser.sleep(6000);
+
+    // browser.pause();
   });
 });
